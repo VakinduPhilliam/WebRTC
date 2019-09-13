74c1251290f5675a0f23ab4c30356293295e3965
Vakindu Philliam <Pvakindu@gmail.com>
2019-09-13 23:03:24 +0300

    Add files via upload

---
+++ react/features/chat/actionTypes.js
@@ -0,0 +1,43 @@
+/**
+ * The type of the action which signals to add a new chat message.
+ *
+ * {
+ *     type: ADD_MESSAGE,
+ *     displayName: string
+ *     hasRead: boolean,
+ *     id: string,
+ *     messageType: string,
+ *     message: string,
+ *     timestamp: string,
+ * }
+ */
+export const ADD_MESSAGE = Symbol('ADD_MESSAGE');
+
+/**
+ * The type of the action which signals to remove all saved chat messages.
+ *
+ * {
+ *     type: CLEAR_MESSAGES
+ * }
+ */
+export const CLEAR_MESSAGES = Symbol('CLEAR_MESSAGES');
+
+/**
+ * The type of the action which signals a send a chat message to everyone in the
+ * conference.
+ *
+ * {
+ *     type: SEND_MESSAGE,
+ *     message: string
+ * }
+ */
+export const SEND_MESSAGE = Symbol('SEND_MESSAGE');
+
+/**
+ * The type of the action which signals to toggle the display of the chat panel.
+ *
+ * {
+ *     type: TOGGLE_CHAT
+ * }
+ */
+export const TOGGLE_CHAT = Symbol('TOGGLE_CHAT');
